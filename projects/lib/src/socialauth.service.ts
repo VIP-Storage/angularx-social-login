d7986a4a4a07686b42a690eb121b917357844f64
Keaton Burleson <keaton@vipindoorstorage.com>
2024-01-31 10:20:13 -0600

    Do not allow dismissing before initializing

--- projects/lib/src/socialauth.service.ts
+++ projects/lib/src/socialauth.service.ts
@@ -160,7 +160,9 @@ export class SocialAuthService {
    */
   dismissAuthPrompt(providerId: string): Promise<void> {
     return new Promise((resolve, reject) => {
-      if (providerId !== GoogleLoginProvider.PROVIDER_ID) {
+      if (!this.initialized) {
+        reject(SocialAuthService.ERR_NOT_INITIALIZED);
+      } else if (providerId !== GoogleLoginProvider.PROVIDER_ID) {
         reject(SocialAuthService.ERR_NOT_SUPPORTED_FOR_DISMISSING_PROMPT);
       } else {
         const providerObject = this.providers.get(providerId);
